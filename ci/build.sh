1bd63e3454adb06df2ceb730b200428c550c5a5d
Jan Kundrát <jan.kundrat@cesnet.cz>
2019-10-10 19:39:37 +0200

    CI: skeleton for hotlinking to a dashboard-under-test
    Change-Id: I8abd2b7858a4f8d311ff23decd971f2f76dc3299

---
+++ ci/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eux -o pipefail
+shopt -s failglob
+
+ZUUL_JOB_NAME=$(jq < ~/zuul-env.json -r '.job')
+
+if [[ $ZUUL_JOB_NAME =~ .*-gammarus-sdn-roadm-line$ ]]; then
+  DEVICE=sdn-roadm-line
+else
+  echo "Cannot determine device name from job name"
+  exit 1
+fi
+
+# this is one of the whitelisted directories
+WEBROOT=~/zuul-output/docs
+mkdir ${WEBROOT}
+
+cat > $WEBROOT/index.html <<EOF
+<html>
+<head><title>dummy CI test for ${DEVICE}</title></head>
+<body><h1>It works: ${DEVICE}</h1></body>
+</html>
+EOF
