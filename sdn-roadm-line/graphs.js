3c752980889e7efa5a1b1a00a9c43d86438fb3ab
Václav Kubernát <kubernat@cesnet.cz>
2019-10-25 16:41:29 +0200

    Draw rectangles
    Change-Id: Ib1400af7390ab1e4ce4182156360ecc78048dfa1

--- sdn-roadm-line/graphs.js
+++ sdn-roadm-line/graphs.js
@@ -9,32 +9,51 @@
  */
 
 /**
- * @typedef Data
+ * @typedef ScanData
  *
  * @property {Value[]} common-in
  * @property {Value[]} common-out
  */
 
-/** @typedef Input
- * @property {Data} czechlight-roadm-device:full-spectrum-scan
+/** @typedef ScanInput
+ * @property {ScanData} czechlight-roadm-device:full-spectrum-scan
  */
 
+/**
+ * @typedef Power
+ *
+ * @property {string} common-in
+ * @property {string} common-out
+ * @property {string} leaf-in
+ * @property {string} leaf-out
+ */
 
-/** @returns {Promise<Input>} */
-const getData = async () => {
-    let url;
-    if (window.location.href.match("logs")) {
-        url = window.location.href + "../../../dummy/sdn-roadm-line/dummy-rpc-full-spectrum-scan.json";
-    } else {
-        url = "/+restconf/operations/czechlight-roadm-device:full-spectrum-scan";
-    }
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw Error(`Data request didn't get a proper response (status=${response.status})`);
-    }
-    const data = await response.json();
-    return data;
-}
+/**
+ * @typedef MediaChannel
+ *
+ * @property {string} channel
+ * @property {Power} power
+ */
+
+/**
+ * @typedef Channel
+ *
+ * @property {string} name
+ * @property {string} lower-frequency
+ * @property {string} upper-frequency
+ */
+
+/**
+ * @typedef ChannelPlan
+ *
+ * @property {Channel[]} channel
+ */
+
+/**
+ * @typedef ChannelInput
+ * @property {MediaChannel[]} czechlight-roadm-device:media-channels
+ * @property {ChannelPlan[]} czechlight-roadm-device:channel-plan
+ */
 
 
 /** @param {Value[]} data */
@@ -45,11 +64,27 @@ const transformData = (data) => data.map((value) => { return {x: value.frequency
 let ctx = document.getElementById('myChart');
 let errorElement = document.getElementById('error');
 
+/** @returns {Promise<ScanInput>} */
+const getScanData = async () => {
+    let url;
+    if (window.location.href.match("logs")) {
+        url = window.location.href + "../../../dummy/sdn-roadm-line/dummy-rpc-full-spectrum-scan.json";
+    } else {
+        url = "/+restconf/operations/czechlight-roadm-device:full-spectrum-scan";
+    }
+    const response = await fetch(url, {cache: "no-store"});
+    if (!response.ok) {
+        throw Error(`Data request didn't get a proper response (status=${response.status})`);
+    }
+    const data = await response.json();
+    return data;
+}
+
 /** @param {Chart} chart */
 const plotScan = async (chart) => {
-        const data = await getData();
+        const data = await getScanData();
 
-        /** @type {Data} */
+        /** @type {ScanData} */
         const innerData = data['czechlight-roadm-device:full-spectrum-scan'];
 
         let commonInData = transformData(innerData['common-in']);
@@ -76,6 +111,81 @@ const plotScan = async (chart) => {
         errorElement.innerText = "";
 }
 
+/** @returns {Promise<ChannelInput>} */
+const getChannelsData = async () => {
+    let url;
+    if (window.location.href.match("logs")) {
+        url = window.location.href + "../../../dummy/sdn-roadm-line/dummy-data-czechlight-roadm-device.json";
+    } else {
+        url = "/+restconf/operations/czechlight-roadm-device:full-spectrum-scan";
+    }
+    const response = await fetch(url, {cache: "no-store"});
+    if (!response.ok) {
+        throw Error(`Data request didn't get a proper response (status=${response.status})`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+/** @param {Chart} chart */
+const plotChannels = async (chart) => {
+    const data = await getChannelsData();
+    console.log(data);
+
+    /** @type ChannelPlan */
+    let channelPlan = data['czechlight-roadm-device:channel-plan'];
+
+    /** @type MediaChannel[] */
+    let mediaChannels = data['czechlight-roadm-device:media-channels'];
+    let template = {
+        drawTime: "beforeDatasetsDraw",
+        type: "box",
+        xScaleID: "xscale",
+        yScaleID: "yscale",
+        yMin: -50,
+        borderWidth: 1
+    }
+
+    // @ts-ignore - for some reason, this is not in the `plugin` property, so TS doesn't know about it
+    chart.options.annotation.annotations = [];
+    // @ts-ignore - for some reason, this is not in the `plugin` property, so TS doesn't know about it
+    let channelOut = mediaChannels.filter((mediaChannel) => mediaChannel.power['leaf-out'] !== undefined).map((mediaChannel) => {
+        let {'upper-frequency': upper, 'lower-frequency': lower} = channelPlan.channel.find((channel) => channel.name == mediaChannel.channel);
+        return {
+            xMin: +lower/1000000,
+            xMax: +upper/1000000,
+            yMax: mediaChannel.power['leaf-out'],
+            borderColor: "red",
+            backgroundColor: "rgba(255,224,224,0.5)",
+
+            ...template
+        };
+    });
+
+    // @ts-ignore - for some reason, this is not in the `plugin`
+    // property, so TS doesn't know about it
+    let channelIn = mediaChannels.filter((mediaChannel) => mediaChannel.power['leaf-in'] !== undefined).map((mediaChannel) => {
+        let {'upper-frequency': upper, 'lower-frequency': lower} = channelPlan.channel.find((channel) => channel.name == mediaChannel.channel);
+        return {
+            xMin: +lower/1000000,
+            xMax: +upper/1000000,
+            yMax: mediaChannel.power['leaf-in'],
+            borderColor: "blue",
+            backgroundColor: "rgba(224,224,255,0.5)",
+
+            ...template
+        };
+    });
+
+    // @ts-ignore - for some reason, this is not in the `plugin`
+    // property, so TS doesn't know about it
+    chart.options.annotation.annotations = [...channelIn, ...channelOut];
+    chart.update();
+
+    ctx.style.backgroundColor = 'rgba(255,0,0,0)';
+    errorElement.innerText = "";
+}
+
 /** @param {Chart} chart */
 const refreshFunction = async (chart) => {
     if (document.hidden) {
@@ -85,6 +195,7 @@ const refreshFunction = async (chart) => {
 
     try {
         plotScan(chart);
+        plotChannels(chart);
     } catch (err) {
         ctx.style.backgroundColor = 'rgba(255,224,224,255)';
         errorElement.innerText = "Error: " + err.message;
@@ -130,6 +241,7 @@ const main = async () => {
             scales: {
                 xAxes: [
                     {
+                        id: "xscale",
                         scaleLabel: {
                             display: true,
                             labelString: "Frequency [THz]"
@@ -141,6 +253,7 @@ const main = async () => {
                 ],
                 yAxes: [
                     {
+                        id: "yscale",
                         scaleLabel: {
                             display: true,
                             labelString: "Power [dBm]"
@@ -149,6 +262,13 @@ const main = async () => {
                             maxRotation: 0
                         }
                     }
+                ]
+            },
+            // @ts-ignore - for some reason, this is not in the `plugin`
+            // property, so TS doesn't know about it
+            annotation: {
+                events: ["click"],
+                annotations: [
                 ]
             },
             plugins: {
