20a37f0b8f6cac1e646c476dbacd417954c8f9fe
Jan Kundrát <jan.kundrat@cesnet.cz>
2020-03-10 16:53:53 -0700

    Power-level graphs for inline amplifiers, too
    Change-Id: I5fa9b592dfc5f25be67e1ee68a6e2b23b36c828c

---
+++ sdn-inline/graphs.js
@@ -0,0 +1,139 @@
+// I would love to define the full JSON structure, but typescript doesn't want
+// colons in property names (although dashes work) and TS type checking is why
+// I use JSDoc anyway.
+/**
+ * @typedef Value
+ *
+ * @property {number} frequency
+ * @property {number} power
+ */
+
+/**
+ * @typedef Data
+ *
+ * @property {Value[]} common-in
+ * @property {Value[]} common-out
+ */
+
+/** @typedef Input
+ * @property {Data} czechlight-coherent-add-drop:*
+ */
+
+
+/** @returns {Promise<Input>} */
+const getData = async () => {
+    let url;
+    if (window.location.pathname.match("/ci-logs-CzechLight-internal/") || true) {
+        url = window.location.href + "../../../dummy/sdn-inline/dummy-data-czechlight-inline-amp.json";
+    } else {
+        url = "/+restconf/data/czechlight-inline-amp:*";
+    }
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw Error(`Data request didn't get a proper response (status=${response.status})`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+
+/** @param {Value[]} data */
+const transformData = (data) => data.map((value) => { return {x: value.frequency / 1000000, y: value.power}; }).sort((a, b) => a.x - b.x);
+
+/** @type {HTMLCanvasElement} */
+// @ts-ignore - getElementById returns HTMLElement and I can't do type assertions (`as`) in JS.
+let ctx = document.getElementById('myChart');
+let errorElement = document.getElementById('error');
+
+/** @param {Chart} chart */
+const refreshFunction = async (chart) => {
+    if (document.hidden) {
+        setTimeout(refreshFunction, 500, chart);
+        return;
+    }
+
+    try {
+        const data = await getData();
+
+        const eastToWest = data['czechlight-inline-amp:east-to-west'];
+        const westToEast = data['czechlight-inline-amp:west-to-east'];
+
+        chart.data.datasets[0].data[0][1] = westToEast['optical-power']['input'];
+        chart.data.datasets[1].data[0][1] = eastToWest['optical-power']['output'];
+        chart.data.datasets[0].data[1][1] = eastToWest['optical-power']['input'];
+        chart.data.datasets[1].data[1][1] = westToEast['optical-power']['output']
+
+        chart.update();
+        ctx.style.backgroundColor = 'rgba(255,0,0,0)';
+        errorElement.innerText = "";
+    } catch (err) {
+        ctx.style.backgroundColor = 'rgba(255,224,224,255)';
+        errorElement.innerText = "Error: " + err.message;
+    }
+    setTimeout(refreshFunction, 500, chart)
+}
+
+const main = async () => {
+    let oldXmin;
+    let oldXmax;
+    let LOW = -100.0;
+
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [
+                'West',
+                'East',
+            ],
+            datasets: [
+                {
+                    label: "Line IN",
+                    backgroundColor: "blue",
+                    data: [[LOW, LOW], [LOW, LOW], ],
+                },
+                {
+                    label: "Line OUT",
+                    backgroundColor: "red",
+                    data: [[LOW, LOW], [LOW, LOW], ],
+                },
+
+            ],
+        },
+        options: {
+            scaleBeginAtZero: false,
+            responsive: true,
+            spanGaps: true,
+            animation: {
+                duration: 0
+            },
+            scales: {
+                xAxes: [
+                    {
+                        gridlines: {
+                            display: false,
+                        },
+                    },
+                ],
+                yAxes: [
+                    {
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Power [dBm]"
+                        },
+                        ticks: {
+                            maxRotation: 0,
+                            min: -60.0,
+                            suggestedMax: 0.0,
+                            beginAtZero: false,
+                        }
+                    },
+                ],
+            },
+        }
+    });
+
+    refreshFunction(myChart);
+}
+
+
+main();
