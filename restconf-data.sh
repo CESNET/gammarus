2e65477197a3c2bb3daf216e593a220d0865ae3c
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-01-08 20:21:02 +0100

    "RESTCONF": don't forget trailing newline
    ...so that it doesn't clutter my shell prompt, please.
    
    Change-Id: Ie0d65840d2bfb670e2d1964208ebdaaf28a9175c

--- restconf-data.sh
+++ restconf-data.sh
@@ -6,5 +6,5 @@ if [[ "${MODULE}" =~ ^[a-zA-Z0-9]([a-zA-Z0-9_-])+$ ]]; then
   echo -en "HTTP/1.1 200 OK\r\nContent-Type: application/yang-data+json\r\nServer: hackish sysrepocfg\r\n\r\n"
   sysrepocfg -f json -d operational -m "${MODULE}" -X
 else
-  echo -en "HTTP/1.1 418 I'm a teapot\r\nContent-Type: text/plain\r\n\r\nmalformed module"
+  echo -en "HTTP/1.1 418 I'm a teapot\r\nContent-Type: text/plain\r\n\r\nmalformed module\n"
 fi
