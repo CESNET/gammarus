6758069670f577439fbb3f813982b8a1344e2eb5
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-01-08 17:54:31 +0100

    "RESTCONF": Do not accidentally leak sensitive data
    This will need an appropriate user defined via br2-external, of course.
    Also, because we are not root anymore, /run is no longer available for
    random writing.
    
    Change-Id: Iad9882c76dfff2d31756f3c3615dc7b520f3ad3f

--- restconf-data.sh
+++ restconf-data.sh
@@ -2,7 +2,9 @@
 read HTTP_REQUEST
 MODULE=$(echo "${HTTP_REQUEST}" | cut -d / -f 4 | cut -d : -f 1)
 
-if [[ "${MODULE}" =~ ^[a-zA-Z0-9]([a-zA-Z0-9_-])+$ ]]; then
+if [[ $(id -u) -eq 0 ]]; then
+  echo -en "HTTP/1.1 501 \r\nContent-Type: text/plain\r\n\r\nmisconfigured uid\n"
+elif [[ "${MODULE}" =~ ^[a-zA-Z0-9]([a-zA-Z0-9_-])+$ ]]; then
   echo -en "HTTP/1.1 200 OK\r\nContent-Type: application/yang-data+json\r\nServer: hackish sysrepocfg\r\n\r\n"
   sysrepocfg -f json -d operational -m "${MODULE}" -X
 else
