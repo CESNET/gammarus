0bd79052ef1c1c1cdc0569ad8f44d29ef2ec082e
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-01-08 20:20:52 +0100

    "RESTCONF": allow exporting full datastore as well
    The datastore contains sensitive info such as the server's private SSH
    key, so this relies on previous commits which make sure that we are no
    longer root.
    
    Change-Id: I5740d2bcc9d413248b378bbd13deb5a6d729dea7

--- restconf-data.sh
+++ restconf-data.sh
@@ -7,6 +7,9 @@ if [[ $(id -u) -eq 0 ]]; then
 elif [[ "${MODULE}" =~ ^[a-zA-Z0-9]([a-zA-Z0-9_-])+$ ]]; then
   echo -en "HTTP/1.1 200 OK\r\nContent-Type: application/yang-data+json\r\nServer: hackish sysrepocfg\r\n\r\n"
   sysrepocfg -f json -d operational -m "${MODULE}" -X
+elif [[ "${HTTP_REQUEST}" = "/+restconf/data" ]]; then
+  echo -en "HTTP/1.1 200 OK\r\nContent-Type: application/yang-data+json\r\nServer: hackish sysrepocfg\r\n\r\n"
+  sysrepocfg -f json -d operational -X
 else
   echo -en "HTTP/1.1 418 I'm a teapot\r\nContent-Type: text/plain\r\n\r\nmalformed module\n"
 fi
