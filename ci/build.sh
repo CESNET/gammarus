8448f07999bcd73ca083bffafcbb083c5097511d
David Mrázek <mrazed00@prf.jcu.cz>
2024-02-12 17:26:29 +0100

    React app boilerplate for CzechLight device dashboard
    Change-Id: If1cc377c8c601f48a491887db18703434fae9e87

--- ci/build.sh
+++ ci/build.sh
@@ -4,22 +4,9 @@ set -eux -o pipefail
 shopt -s failglob
 
 ZUUL_JOB_NAME=$(jq < ~/zuul-env.json -r '.job')
-CI_PARALLEL_JOBS=$(awk -vcpu=$(getconf _NPROCESSORS_ONLN) 'BEGIN{printf "%.0f", cpu*1.3+1}')
 
 # this is one of the whitelisted directories
 WEBROOT=~/zuul-output/docs
-HTML=${WEBROOT}/index.html
 mkdir -p ${WEBROOT}
-
-make -j${CI_PARALLEL_JOBS} install DESTDIR=.OUT
-tree .OUT > tree-output
-echo '<title>Dashboards demos for all devices</title><meta charset="utf-8"/><ul>' > ${HTML}
-for ITEM in .OUT/usr/share/gammarus/static/* ; do
-  mv ${ITEM} ${WEBROOT}/
-  DEVICE=$(basename ${ITEM})
-  echo "<li><a href='${DEVICE}/index.html'>$DEVICE</a></li>" >> ${HTML}
-done
-echo '</ul><hr/><pre>' >> ${HTML}
-cat tree-output >> ${HTML}
-echo '</html>' >> ${HTML}
-cp -a dummy ${WEBROOT}/
+npm ci
+BUILD_DIR=${WEBROOT} npm run build 
